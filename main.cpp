7470b1ce9e5e16c516a41a85b58e1039935e922e
Myth <lxbpxylps@126.com>
2023-02-05 21:02:19 +0800

    chore: bump version to 0.12

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QApplication::setApplicationName("ZJU Connect for Windows");
-    QApplication::setApplicationVersion("0.11");
+    QApplication::setApplicationVersion("0.12");
 
     MainWindow mainWindow;
     mainWindow.show();
